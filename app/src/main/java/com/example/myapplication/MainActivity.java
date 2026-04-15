87012e06da15f2d36fb622d2c7b13ff616a10630
Maria <mariaisabelkleindelavy@gmail.com>
2026-04-14 22:28:21 -0300

    Calcular imc

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -14,53 +14,26 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView textView;
-    EditText etMin;
-    EditText etMax;
-    Button button;
-    Button btnreset;
-    Button btnrnd;
-    int c=0;
+    Button b;
+    EditText edpeso, edaltura;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        b=findViewById(R.layout.button);
+        edaltura=findViewById(R.id.edAltura);
+        edpeso=findViewById(R.id.edPeso);
 
-        textView=findViewById(R.id.tv);
-        etMin=findViewById(R.id.etMin);
-        etMax=findViewById(R.id.etMax);
-        button=findViewById(R.id.button);
-        btnreset=findViewById(R.id.btnreset);
-        btnrnd=findViewById(R.id.btnrdn);
-        textView.setText("0");
+        b.setOnClickListener((v -> {
+            Intent intent = new Intent(this, IMCResultado.class);
+            //passar os dados para o bundle
+            float peso = Float.parseFloat(edpeso.getText().toString());
+            float altura = Float.parseFloat(edaltura.getText().toString());
 
-        // Botão de incremento
-        button.setOnClickListener(v -> {
-            c++;
-            textView.setText(Integer.toString(c));
-        });
+        }));
 
-        // Botão de reset
-        btnreset.setOnClickListener(v -> {
-            textView.setText("0");
-            c=0;
-            etMin.setText("0");
-            etMax.setText("100");
-        });
 
-        // Botão de número aleatório
-        btnrnd.setOnClickListener(v -> {
-            String minText = etMin.getText().toString();
-            String maxText = etMax.getText().toString();
-            int min = Integer.parseInt(etMin.getText().toString());
-            int max = Integer.parseInt(etMax.getText().toString());
-
-            Random random = new Random();
-            int randomNumber = random.nextInt((max - min) + 1) + min;
-
-            textView.setText(Integer.toString(randomNumber));
-            c = randomNumber;
-        });
     }
 }
