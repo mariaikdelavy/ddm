3d9c2bd185ef6caaa266044c3fb31eceb93f339c
mariaikdelavy <mariaisabelkleindelavy@gmail.com>
2026-04-15 17:02:34 -0300

    IMC/2

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import java.text.DecimalFormat;
 import java.util.Random;
 
 import androidx.activity.EdgeToEdge;
@@ -11,26 +13,59 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
     Button b;
     EditText edpeso, edaltura;
+    TextView tvIMC;
+    ImageView imageView;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        b=findViewById(R.layout.button);
+        b=findViewById(R.id.button);
         edaltura=findViewById(R.id.edAltura);
         edpeso=findViewById(R.id.edPeso);
+        tvIMC = findViewById(R.id.tvIMC);
+        imageView = findViewById(R.id.imageView);
 
         b.setOnClickListener((v -> {
-            Intent intent = new Intent(this, IMCResultado.class);
-            //passar os dados para o bundle
-            float peso = Float.parseFloat(edpeso.getText().toString());
-            float altura = Float.parseFloat(edaltura.getText().toString());
+            Double peso=Double.parseDouble(edpeso.getText().toString());
+            Double altura=Double.parseDouble(edaltura.getText().toString());
+            double imc = peso/(altura * altura);
+            DecimalFormat dc = new DecimalFormat("##.##");
+            tvIMC.setText(dc.format(imc));
+
+            if(imc < 19){
+                imageView.setImageResource(R.drawable.abaixopeso);
+                return;
+            }
+
+            if(imc < 25){
+                imageView.setImageResource(R.drawable.normal);
+                return;
+            }
+
+            if(imc < 30){
+                imageView.setImageResource(R.drawable.sobrepeso);
+                return;
+            }
+
+            if(imc < 34){
+                imageView.setImageResource(R.drawable.obesidade1);
+                return;
+            }
+
+            if(imc < 40){
+                imageView.setImageResource(R.drawable.obesidade2);
+                return;
+            }
+
+            imageView.setImageResource(R.drawable.obesidade3);
 
         }));
 
