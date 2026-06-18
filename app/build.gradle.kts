a596fe87415e188f9af1a72a8806191c6858f078
mariaikdelavy <mariaisabelkleindelavy@gmail.com>
2026-06-17 22:03:10 -0300

    Início aplicação camera

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,9 +2,10 @@ plugins {
     alias(libs.plugins.android.application)
 }
 
+
 android {
     namespace = "com.example.myapplication"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.example.myapplication"
@@ -37,7 +38,21 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
-    implementation("org.osmdroid:osmdroid-android:6.1.16")
+    //DEPENDECIAS CAMERAX
+    //implementation(libs.camerax.core)
+    //implementation(libs.camerax.camera2)
+    //implementation((libs.camerax.))
+    // CameraX core library using the camera2 implementation
+    val camerax_version = "1.3.4"
+    // The following line is optional, as the core library is included indirectly by camera-camera2
+    implementation("androidx.camera:camera-core:${camerax_version}")
+    implementation("androidx.camera:camera-camera2:${camerax_version}")
+    // If you want to additionally use the CameraX Lifecycle library
+    implementation("androidx.camera:camera-lifecycle:${camerax_version}")
+    // If you want to additionally use the CameraX VideoCapture library
+    implementation("androidx.camera:camera-video:${camerax_version}")
+    // If you want to additionally use the CameraX View class
+    implementation("androidx.camera:camera-view:${camerax_version}")
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,112 +1,89 @@
 package com.example.myapplication;
 
+import static androidx.camera.core.CameraSelector.DEFAULT_FRONT_CAMERA;
+
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.camera.core.CameraSelector;
+import androidx.camera.core.Preview;
+import androidx.camera.lifecycle.ProcessCameraProvider;
+import androidx.camera.view.PreviewView;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
-import org.osmdroid.config.Configuration;
-import org.osmdroid.util.GeoPoint;
-import org.osmdroid.views.MapView;
-import org.osmdroid.views.overlay.Marker;
+import com.google.common.util.concurrent.ListenableFuture;
+
+import java.util.concurrent.ExecutionException;
+
 
 public class MainActivity extends AppCompatActivity {
 
-    private LocationManager locationManager;
-    private TextView tv;
-    private Button buttonLoc;
-    private MapView mapView;
-    private Marker marcadorPosicaoAtual;
-    private static final int PERMISSION_REQUEST_CODE = 1;
+    ListenableFuture<ProcessCameraProvider> cameraProviderFuture;
+    PreviewView previewView;
+    ImageButton imageButton;
 
+    private CameraSelector cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA;
+    private ProcessCameraProvider cameraProvider;
+
+    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        tv = findViewById(R.id.textview);
-        buttonLoc = findViewById(R.id.buttonLoc);
-        mapView = findViewById(R.id.mapView);
 
-        Configuration.getInstance().setUserAgentValue(getPackageName());
+        previewView = findViewById(R.id.PreviewView);
+        imageButton = findViewById(R.id.button);
 
-        mapinit();
-        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        if(checkAndRequestPermission()){
+            cameraProviderFuture = ProcessCameraProvider.getInstance(this);
+            cameraProviderFuture.addListener(() -> {
+                try{
+                    ProcessCameraProvider cameraProvider = cameraProviderFuture.get();
+                    bindPreview(cameraProvider);
+                } catch (ExecutionException | InterruptedException e){
 
-        // Configura o clique do botão
-        buttonLoc.setOnClickListener(v -> {
-            if (hasLocationPermission()) {
-                getLocation();
-            } else {
-                checkLocationPermission();
-            }
+                }
+            }, ContextCompat.getMainExecutor(this));
+        }
+        imageButton.setOnClickListener(v -> {
+            //obtem camera selector
+            trocaCamera();
+            //obtem camera provider
+            ProcessCameraProvider cameraProvider= null;
         });
     }
 
-    // Método auxiliar para checar se a permissão já existe
-    private boolean hasLocationPermission() {
-        return ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED;
+    private void bindPreview(ProcessCameraProvider cameraProvider) {
+        Preview preview = new Preview.Builder().build();
+        cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA;
+        preview.setSurfaceProvider(previewView.getSurfaceProvider());
+        cameraProvider.bindToLifecycle(this, cameraSelector, preview);
     }
 
-    private void getLocation() {
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            return;
-        }
-
-        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, location -> {
-            tv.setText("Latitude: " + location.getLatitude() +
-                        "\nLongitude: " + location.getLongitude() +
-                        "\nAltitude: " + location.getLongitude() +
-                        "\nVelocidade: " + location.getSpeed());
-                showLocationOnMap(location);
-        });
-
-    }
-
-    public void mapinit(){
-        mapView.setMultiTouchControls(true);
-    }
-
-    public void showLocationOnMap(Location location){
-        double latidude = location.getLatitude();
-        double longitude = location.getLongitude();
-        GeoPoint mLocation = new GeoPoint(latidude, longitude);
-        mapView.getController().setCenter(mLocation);
-        mapView.getController().setCenter(mLocation);
-        mapView.getController().setZoom(18.0);
-        mapView.getController().animateTo(mLocation);
-        //adiciona um overlay para mostrar localização atual (marker)
-        if(marcadorPosicaoAtual == null){
-            marcadorPosicaoAtual = new Marker(mapView);
-        }
-        marcadorPosicaoAtual.setPosition(mLocation);
-        marcadorPosicaoAtual.setTitle("Minha localização");
-        mapView.getOverlays().add(marcadorPosicaoAtual);
-    }
-    private void checkLocationPermission() {
-        if (!hasLocationPermission()) {
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSION_REQUEST_CODE);
+    public CameraSelector trocaCamera(){
+        if(cameraSelector.equals(CameraSelector.DEFAULT_BACK_CAMERA)){
+            return DEFAULT_FRONT_CAMERA;
+        }else{
+            return CameraSelector.DEFAULT_BACK_CAMERA;
         }
     }
 
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-
-        if (requestCode == PERMISSION_REQUEST_CODE) {
-            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                getLocation();
-            } else {
-                Toast.makeText(this, "Permissão de localização necessária.", Toast.LENGTH_SHORT).show();
-            }
+    public boolean checkAndRequestPermission(){
+        if(ActivityCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.CAMERA}, 100);
+            return false;
         }
+        return  true;
     }
 }
